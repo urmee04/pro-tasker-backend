ccc15aaebc60865075eb43c553eda239447b203a
urmee04 <urmee04sust@gmail.com>
2025-09-03 23:42:02 -0400

    fix taskRoutes path

--- routes/api/index.js
+++ routes/api/index.js
@@ -7,9 +7,9 @@ const projectRoutes = require("./projectRoutes");
 const taskRoutes = require("./taskRoutes");
 
 //define API routes
-router.use("/api/users", userRoutes); //user management routes
-router.use("/api/projects", projectRoutes); //project management routes
-router.use("/api", taskRoutes); //task management routes (nested under projects and standalone tasks)
+router.use("/users", userRoutes); //user management routes
+router.use("/projects", projectRoutes); //project management routes
+router.use(taskRoutes); //task management routes (nested under projects and standalone tasks)
 
 //export the router for use in the main application
 module.exports = router;
