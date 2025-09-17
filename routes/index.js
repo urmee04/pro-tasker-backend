201fc8f5432155ee7b3172e71059d3d43758dffa
urmee04 <urmee04sust@gmail.com>
2025-09-17 11:52:29 -0400

    remove api prefix to resolve 404 error

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const apiRoutes = require("./api");
 
 //API routes - all routes in the api directory will be prefixed with /api
-router.use("/api", apiRoutes);
+router.use(apiRoutes);
 //404 handler for any requests to undefined routes
 router.use((req, res) => {
   res.status(404).send("404 Error!");
